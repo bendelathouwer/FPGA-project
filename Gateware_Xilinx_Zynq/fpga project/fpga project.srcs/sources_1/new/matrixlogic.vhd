a9d705a3cc15308e8bfd0cff31cad7bed11a88da
Ben De Lathouwer <bendelathouwer@users.noreply.github.com>
2026-04-29 13:48:22 +0200

    added matrix logic VHDL file

---
+++ Gateware_Xilinx_Zynq/fpga project/fpga project.srcs/sources_1/new/matrixlogic.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 29.04.2026 12:01:10
+-- Design Name: 
+-- Module Name: matrixlogic - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity matrixlogic is
+--  Port ( );
+end matrixlogic;
+
+architecture Behavioral of matrixlogic is
+
+begin
+
+
+end Behavioral;
