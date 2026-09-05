e6e27cbe3181a91673527f2024b537faae945f8d
Ben De Lathouwer <bendelathouwer@users.noreply.github.com>
2026-09-05 14:12:37 +0200

    driver
    started fucking about with the matrix gateware, lost te plot and deleted it

---
+++ Gateware_Xilinx_Zynq/fpga_Matrix/fpga project.srcs/sources_1/new/matrixlogic.vhd
@@ -0,0 +1,65 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 29.04.2026 12:01:10
+-- Design Name: 
+-- Module Name: matrixlogic - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity hub75E_driver is
+ generic (
+        COLS : integer := 64;
+        ROWS : integer := 64
+    );
+ Port (     			 
+            clockIN_matrix  : in std_logic;--clock into the system
+            --ditherd pixeldata 
+            R_in_matrix : out std_logic_vector(2 downto 0);
+            G_in_matrix  : out std_logic_vector(2 downto 0);
+            B_in_matrix  : out std_logic_vector(1 downto 0);
+			 clockOUt_matrix  : out std_logic;--clock into the system
+            --control logic  to the pannel
+            R1, G1, B1, R2, G2, B2 : out std_logic;
+            A, B, C, D, E : out std_logic;
+            LAT, OE, matrix_latch_clock_IN: out std_logic--matrix_clockIN is actualy the shift registers latch 
+			);
+end hub75E_driver;
+
+architecture Behavioral of hub75E_driver is
+
+begin
+    --here we start the process with our matrixclock we derive from the top file (top file->clk div -> matrix logic0
+    process(clockIN_matrix)
+    begin
+        --we now trigger the code on the rising edge of the matrix clock 
+        if rising_edge (clockIN_matrix) then
+        clockOUt_matrix<=clockIN_matrix;
+        end if ;     
+    end process;
+
+end Behavioral;
---
+++ Gateware_Xilinx_Zynq/pcie-spf/project_1.srcs/sources_1/new/matrix_logic.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09/03/2026 07:19:11 PM
+-- Design Name: 
+-- Module Name: matrix_logic - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity matrix_logic is
+--  Port ( );
+end matrix_logic;
+
+architecture Behavioral of matrix_logic is
+
+begin
+
+
+end Behavioral;
